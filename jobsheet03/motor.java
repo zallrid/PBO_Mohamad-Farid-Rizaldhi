fd675fd70fbfef6a0db511cfc54d852242a7f4a5
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2026-09-07 14:11:00 +0700

    praktikum 3

--- jobsheet03/motor.java
+++ jobsheet03/motor.java
@@ -1,14 +1,43 @@
 package jobsheet03;
 
 public class motor {
-    public String platNomor;
-    public boolean statusMesin;
-    public int kecepatan;
+    private String platNomor;
+    private boolean statusMesin;
+    private int kecepatan;
 
-    public void displayInfo(){
+    public void displayInfo() {
         System.out.println("Plat Nomor: " + this.platNomor);
         System.out.println("Status Mesin: " + (this.statusMesin ? "On" : "Off"));
         System.out.println("Kecepatan: " + this.kecepatan);
         System.out.println("===========================");
+    }
+
+    public String getPlatNomor() {
+        return platNomor;
+    }
+
+    public void setPlatNomor(String platNomor) {
+        this.platNomor = platNomor;
+    }
+
+    public boolean getstatusMesin() {
+        return statusMesin;
+    }
+
+    public void setstatusMesin(boolean statusMesin) {
+        this.statusMesin = statusMesin;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+       if (!this.statusMesin && kecepatan > 0){
+            System.out.println("kecepatan tidak boleh lebih dari 0 jika mesin off");
+       } else {
+           this.kecepatan = kecepatan;
+       }
+
     }
 }
--- jobsheet03/motorDemo.java
+++ jobsheet03/motorDemo.java
@@ -3,40 +3,17 @@ package jobsheet03;
 public class motorDemo {
     public static void main(String[] args) {
         motor motor1 = new motor();
-        motor1.platNomor = "B 1234 XZ";
-        int kecepatanBaru = 50;
-
-        if (!motor1.statusMesin && kecepatanBaru > 0){
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        }
-        else{
-            motor1.kecepatan = kecepatanBaru;
-        }
+        motor1.setPlatNomor("B 0838 XZ");
+        motor1.setKecepatan(50);
         motor1.displayInfo();
-
         motor motor2 = new motor();
-        motor2.platNomor = "N 9840 AB";
-        motor2.statusMesin = true;
-        kecepatanBaru = 60;
-
-        if (!motor2.statusMesin && kecepatanBaru > 0){
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        }
-        else{
-            motor1.kecepatan = kecepatanBaru;
-        }
+        motor2.setPlatNomor("N 9840 AB");
+        motor2.setstatusMesin(true);
+        motor2.setKecepatan(40);
         motor2.displayInfo();
-
         motor motor3 = new motor();
-        motor3.platNomor = "D 8343 CV";
-        kecepatanBaru = 40;
-
-        if (!motor3.statusMesin && kecepatanBaru > 0){
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        }
-        else{
-            motor1.kecepatan = kecepatanBaru;
-        }
+        motor3.setPlatNomor("D 8343 CV");
+        motor3.setKecepatan(60);
         motor3.displayInfo();
     }
 }
